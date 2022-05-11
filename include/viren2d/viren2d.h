0371e5202e2a8a0789c72c8a4d24b0c6b7e173ed
snototter <snototter@users.noreply.github.com>
2022-05-11 17:34:26 +0200

    include public logging interface

--- include/viren2d/viren2d.h
+++ include/viren2d/viren2d.h
@@ -3,8 +3,9 @@
 
 #include <viren2d/colors.h>
 #include <viren2d/drawing.h>
-#include <viren2d/math.h>
-#include <viren2d/string_utils.h>
 #include <viren2d/primitives.h>
+#include <viren2d/styles.h>
+#include <viren2d/logging.h>
+#include <viren2d/version.h>
 
 #endif // __VIREN2D_VIREN2D_H__
