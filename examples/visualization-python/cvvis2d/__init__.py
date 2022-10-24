1545ec260f14cf30e1479d0e28a50f5fbf9f8745
snototter <snototter@users.noreply.github.com>
2022-10-24 21:36:57 +0200

    move visualization pipeline into separate python utility package

--- examples/visualization-python/cvvis2d/__init__.py
+++
@@ -1,8 +0,0 @@
-# The core visualization pipeline
-from cvvis2d.pipeline import VisualizationPipeline
-
-# Text overlays & utils
-from cvvis2d.text_overlay import frame_label, TextOverlay, StaticTextOverlay
-
-# Image overlays
-from cvvis2d.image_overlay import ImageOverlay
